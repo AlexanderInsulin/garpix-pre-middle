bf77d76bf83345bba3ed918bfec56425d02ab6d4
AlexanderInsulin <insulin@protonmail.com>
2018-02-12 19:00:54 +0300

    Add types for actions.

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+export const ADD_PHOTO = 'ADD_PHOTO';
+export const UPLOAD_PHOTO = 'UPLOAD_PHOTO';
+export const RENAME_PHOTO = 'RENAME_PHOTO';
+export const DELETE_PHOTO = 'DELETE_PHOTO';
+export const ADD_ALBUM = 'ADD_ALBUM';
+export const RENAME_ALBUM = 'RENAME_ALBUM';
+export const DELETE_ALBUM = 'DELETE_ALBUM';
