2d4fcde1f1760d5dbf444f31ba523f68a409e70c
AlexanderInsulin <insulin@protonmail.com>
2018-02-13 02:16:29 +0300

    Added and test rename photo in album action creator.

--- src/actions/albumActions.js
+++ src/actions/albumActions.js
@@ -1,7 +1,12 @@
 import { ADD_PHOTO } from './types';
-import { addPhoto } from './photoActions';
+import { addPhoto, renamePhoto } from './photoActions';
 
 export const addPhotoToAlbum = (albumUuid, photoName, photoURL) => ({
   albumUuid: albumUuid,
   ...addPhoto(photoName, photoURL)
+})
+
+export const renamePhotoInAlbum = (albumUuid, photoUuid, newName) => ({
+  albumUuid: albumUuid,
+  ...renamePhoto(photoUuid, newName)
 })
--- src/actions/albumActions.test.js
+++ src/actions/albumActions.test.js
@@ -1,6 +1,6 @@
 import { ADD_PHOTO } from './types';
-import { addPhotoToAlbum } from './albumActions';
-import { addPhoto } from './photoActions';
+import { addPhotoToAlbum, renamePhotoInAlbum } from './albumActions';
+import { addPhoto, renamePhoto } from './photoActions';
 
 
 describe('test galery action creators', () => {
@@ -12,6 +12,17 @@ describe('test galery action creators', () => {
     expect(addPhotoToAlbum(uuid, photoName, photoURL)).toEqual({
       albumUuid: uuid,
       ...addPhoto(photoName, photoURL)
+    })
+  });
+
+  it('should rename photo in album', () => {
+    let albumUuid = 1;
+    let photoUuid = 10;
+    let newName = 'new name';
+    let photoURL = 'image';
+    expect(renamePhotoInAlbum(albumUuid, photoUuid, newName)).toEqual({
+      albumUuid: albumUuid,
+      ...renamePhoto(photoUuid, newName)
     })
   });
 });
