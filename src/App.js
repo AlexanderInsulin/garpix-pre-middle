c68cc97daf75916345dcec0c9bd0d4307be119f2
zyec32 <njkmrjpfzw@mail.ru>
2018-02-13 19:43:47 +0300

    add galery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import Galery from './components/Galery';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar title="head" />
+        <Galery />
       </div>
     );
   }
---
+++ src/components/Galery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Row } from 'reactstrap'
+
+const Galery = () => (
+  <Container>
+    <Row>
+    </Row>
+  </Container>
+)
+
+export default Galery;
