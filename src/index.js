ac390847bcde337c26a02775a6ff2048993c3c82
AlexanderInsulin <insulin@protonmail.com>
2018-02-13 16:35:28 +0300

    Add load and subscrube redux tree to localstorage.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import reducers from './reducers';
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { loadState, saveState } from './utils/storageState';
 
+const persistedState = loadState();
 const store = createStore(
   reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+})
 
 ReactDOM.render(
   <Provider store={store}>
