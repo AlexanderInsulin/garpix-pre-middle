b29feb44f790851576ea48a48dcb2b68b9d3f86b
zyec32 <njkmrjpfzw@mail.ru>
2018-02-16 17:13:18 +0300

    Add AddImage component.

---
+++ src/components/AddAlbum.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+
+class AddAlbum extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button color="danger" onClick={this.toggle}>Открыть модалку</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Создание альбома</ModalHeader>
+          <ModalBody>
+            <Input type="text" placeholder="Название альбома" />
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Создать</Button>
+            <Button color="secondary" onClick={this.toggle}>Отменить</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default AddAlbum;
