46e63cc436bf3b2f6f454abe7089525f7df57f80
zyec32 <njkmrjpfzw@mail.ru>
2018-02-21 23:46:52 +0300

    Fix Navbar style code.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,17 @@ import { Navbar as NavbarBootstrap, NavbarBrand } from 'reactstrap';
 
 const Navbar = ({ title }) => (
   <NavbarBootstrap className="navbarCustom" dark sticky="top">
-    <div className="rightBorder float-left"> <NavbarBrand href="/" > <i className="material-icons navIcone">chevron_left</i> </NavbarBrand> </div>
+    <div className="rightBorder float-left">
+      <NavbarBrand href="/" >
+        <i className="material-icons navIcone">chevron_left</i>
+      </NavbarBrand>
+    </div>
     <div className="navCenter"> {title} </div>
-    <div> <NavbarBrand href="/" className="navEditter"> <i className="material-icons">create</i> </NavbarBrand> </div>
+    <div>
+      <NavbarBrand href="/" className="navEditter">
+        <i className="material-icons">create</i>
+      </NavbarBrand>
+    </div>
   </NavbarBootstrap>
 );
 
