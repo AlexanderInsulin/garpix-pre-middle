7bc8583eb1f0bc8471d1e9e17fccd6da5eeb3cb7
AlexanderInsulin <insulin@protonmail.com>
2018-02-23 02:54:53 +0300

    Add upload photo action creators to actions module.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import * as album from './albumActions';
 import * as photo from './photoActions';
+import * as uploadPhoto from './photoActions';
 import * as types from './types';
 
-export default {album, photo, types}
+export default {album, photo, types, uploadPhoto}
