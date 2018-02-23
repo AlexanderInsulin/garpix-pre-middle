3bdabdeca5db329f6437c0f68698720a020fdd15
NYMAX <nymax.ip@gmail.com>
2018-02-23 14:36:01 +0300

    Fix navbar titile size; refactor icon classes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = ({ title }) => (
   <NavbarBootstrap className="navbarCustom" dark sticky="top">
     <div className="rightBorder float-left">
       <NavbarBrand href="/" >
-        <i className="material-icons navIcone">chevron_left</i>
+        <i className="material-icons navLeftIcon">chevron_left</i>
       </NavbarBrand>
     </div>
     <div className="navCenter"> {title} </div>
     <div>
       <NavbarBrand href="/" className="navEditter">
-        <i className="material-icons">create</i>
+        <i className="material-icons ">create</i>
       </NavbarBrand>
     </div>
   </NavbarBootstrap>
