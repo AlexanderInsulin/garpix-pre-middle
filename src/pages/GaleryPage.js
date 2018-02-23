1186e892e57ba8ad8e164fcdd609a34f9291d9fd
AlexanderInsulin <insulin@protonmail.com>
2018-02-23 05:49:01 +0300

    Now galery page show saved albums.

--- src/pages/GaleryPage.js
+++ src/pages/GaleryPage.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Container, Row } from 'reactstrap'
 import Album from '../components/Album';
+import { connect } from 'react-redux'
 import Navbar from '../components/Navbar';
 
-const GaleryPage = () => (
+const GaleryPage = ({albums}) => (
     <Container>
       <Row>
-        <Album name="album 1" image="https://pp.userapi.com/c831209/v831209113/22953/iERgDLPebI0.jpg" />
-        <Album name="album 2" image="https://pp.userapi.com/c831209/v831209113/22953/iERgDLPebI0.jpg" />
+        {albums.map(album => <Album name={album.name} image="https://pp.userapi.com/c831209/v831209113/22953/iERgDLPebI0.jpg" />)}
         <Album />
       </Row>
     </Container>
 )
 
-export default GaleryPage;
+const mapStateToProps = (state) => ({
+    albums: state.galery
+})
+export default connect(mapStateToProps)(GaleryPage);
