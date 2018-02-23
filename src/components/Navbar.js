c689b3d48210f206164291e7ab243de6a0796fa8
NYMAX <nymax.ip@gmail.com>
2018-02-23 16:08:42 +0300

    Ok, refactor again navbar + finished left content layer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,24 @@ import { Navbar as NavbarBootstrap, NavbarBrand } from 'reactstrap';
 
 const Navbar = ({ title }) => (
   <NavbarBootstrap className="navbarCustom" dark sticky="top">
-    <div className="rightBorder float-left">
-      <NavbarBrand href="/" >
-        <i className="material-icons navLeftIcon">chevron_left</i>
+
+      <NavbarBrand href="/" className="customBrandStyle leftContent">
+        <i className="material-icons navBackIcon">chevron_left</i>
       </NavbarBrand>
-    </div>
-    <div className="navCenter"> {title} </div>
-    <div>
-      <NavbarBrand href="/" className="navEditter">
-        <i className="material-icons ">create</i>
-      </NavbarBrand>
-    </div>
+
+  <div className="leftBorder"></div>
+
+
+  <div className="navCenter"> {title} </div>
+  <div>
+
+
+    <NavbarBrand href="/" className="navEditter">
+       <i className="material-icons ">create</i>
+    </NavbarBrand>
+
+
+  </div>
   </NavbarBootstrap>
 );
 
