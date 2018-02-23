2e4278463794d01400486e11819f60cfc66e224e
AlexanderInsulin <insulin@protonmail.com>
2018-02-23 14:36:42 +0300

    Add galery page.

--- src/pages/GaleryPage.js
+++ src/pages/GaleryPage.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Container, Row } from 'reactstrap'
 import Album from '../components/Album';
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import Navbar from '../components/Navbar';
+import actions from '../actions';
 
-const GaleryPage = ({albums}) => (
+const GaleryPage = ({albums, addAlbum}) => (
     <Container>
       <Row>
-        {albums.map(album => <Album name={album.name} image="https://pp.userapi.com/c831209/v831209113/22953/iERgDLPebI0.jpg" />)}
+        {albums.map((album, key) =>
+          <Album key={key} name={album.name} image="https://pp.userapi.com/c831209/v831209113/22953/iERgDLPebI0.jpg"/>
+        )}
         <Album />
       </Row>
     </Container>
@@ -16,4 +20,6 @@ const GaleryPage = ({albums}) => (
 const mapStateToProps = (state) => ({
     albums: state.galery
 })
+
+
 export default connect(mapStateToProps)(GaleryPage);
