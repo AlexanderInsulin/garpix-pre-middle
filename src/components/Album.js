da53e55e3de60f63c39a09be9f80c2cac2e280c8
AlexanderInsulin <insulin@protonmail.com>
2018-02-23 18:38:37 +0300

    Connect labum to redux.

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import './Album.css';
 import { Col } from 'reactstrap';
 import AddAlbumModal from './AddAlbumModal';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Redirect } from 'react-router';
 
 const createImage = 'https://pp.userapi.com/c834301/v834301478/ae5a9/VrmQSB3NPeU.jpg';
 
 const showImage = (name, image, sizeStyle, toglle) => (
-  <div className={"card " + sizeStyle} style={{ backgroundImage: `url(${image})` }} onClick={toglle}>
-    <div className="filler"> </div>
-    <div className="text"> {name} </div>
-  </div>
+    <div className={"card " + sizeStyle} style={{ backgroundImage: `url(${image})` }} onClick={toglle}  href="/album">
+      <div className="filler"> </div>
+      <div className="text"> {name} </div>
+    </div>
 );
 
 class Album extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      uuid: this.props.uuid,
       name: this.props.name,
       image: this.props.image,
       modalOpen: false,
@@ -30,10 +33,21 @@ class Album extends Component {
     });
   }
 
+  handleOnClick = () => {
+    if (this.state.image) {
+      this.setState({redirect: true});
+    }
+  }
+
   render() {
+    if (this.state.redirect) {
+    return <Redirect push to={"/album/" + this.state.uuid} />;
+  }
     return (
-      <Col xs="12" md="6" xl="4">
-        {this.state.image ? showImage(this.state.name, this.state.image, "backSize") : showImage('', createImage, "backSizeCreate", this.toggle)}
+      <Col xs="12" md="6" xl="4" onClick={this.handleOnClick}>
+        {this.state.image ?
+          showImage(this.state.name, this.state.image, "backSize") :
+          showImage('', createImage, "backSizeCreate", this.toggle)}
         <AddAlbumModal open={this.state.modalOpen} toggle={this.toggle}/>
       </Col>
     );
