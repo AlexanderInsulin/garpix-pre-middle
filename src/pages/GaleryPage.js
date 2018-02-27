7045bfc0bd60651b7bc809d06c93570a9a49ed80
zyec32 <njkmrjpfzw@mail.ru>
2018-02-27 16:15:25 +0300

    First album image.

--- src/pages/GaleryPage.js
+++ src/pages/GaleryPage.js
@@ -11,8 +11,9 @@ import created_album from '../created_album.svg';
 const GaleryPage = ({albums, addAlbum}) => (
     <Container>
       <Row>
+      {console.log(albums)}
         {albums.map((album, key) =>
-          <Album key={key} uuid={album.uuid} name={album.name} image={created_album}/>
+          <Album key={key} uuid={album.uuid} name={album.name} image={album.photos.length == 0 ? created_album : album.photos[0].imageURL}/>
         )}
         <Album />
       </Row>
